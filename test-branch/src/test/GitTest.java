2c210036d2ac60ff7eb3a0e0a6b0ae4a8f8c931b
leejoonsung <leejs198@gmail.com>
2016-11-22 14:18:22 +0900

    add "김밥"

---
+++ test-branch/src/test/GitTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.ArrayList;
+
+public class GitTest {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("김밥");
+	}
+}
